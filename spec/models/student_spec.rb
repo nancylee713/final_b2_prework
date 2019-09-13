a4058280cb63c2691232a379036642841a8de5b4
Nancy Lee <srlee713@gmail.com>
2019-09-13 10:03:56 -0600

    Add validate presence of grade in student model

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Student, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :grade }
     it { should validate_numericality_of(:grade).only_integer }
     it { should validate_inclusion_of(:grade).in_range(0..100) }
   end

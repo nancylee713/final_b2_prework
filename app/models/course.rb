dd7a1db1ba2106c9b115b4d53ed35f85cc76717e
Nancy Lee <srlee713@gmail.com>
2019-09-13 10:02:21 -0600

    Add has_many and belongs_to relationships in three models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,6 @@
 class Course < ApplicationRecord
   validates_presence_of :name
+
+  has_many :course_students
+  has_many :students, through: :course_students
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,7 @@ class Student < ApplicationRecord
   validates_presence_of :name, :grade
   validates_numericality_of :grade, only_integer: true
   validates_inclusion_of :grade, in: 0..100
+
+  has_many :course_students
+  has_many :courses, through: :course_students
 end

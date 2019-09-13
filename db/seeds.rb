bc5f19dc9708d18fde7236572cfe4fe29800d53b
Nancy Lee <srlee713@gmail.com>
2019-09-13 12:13:22 -0600

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,21 @@ Course.destroy_all
 Student.destroy_all
 
 pete = Student.create!(name: "Pete")
+sarah = Student.create!(name: "Sarah")
+john = Student.create!(name: "John")
+
 math = Course.create!(name: "Calculus I")
-math_pete = CourseStudent.create!(course_id: math.id, student_id: pete.id, grade: 90)
-
 english = Course.create!(name: "Compsition 101")
-english_pete = CourseStudent.create!(course_id: english.id, student_id: pete.id, grade: 88)
-
 world_history = Course.create!(name: "World History")
+
+math_pete = CourseStudent.create!(course_id: math.id, student_id: pete.id, grade: 90)
+english_pete = CourseStudent.create!(course_id: english.id, student_id: pete.id, grade: 88)
 world_history_pete = CourseStudent.create!(course_id: world_history.id, student_id: pete.id, grade: 93)
+
+math_sarah = CourseStudent.create!(course_id: math.id, student_id: sarah.id, grade: 100)
+english_sarah = CourseStudent.create!(course_id: english.id, student_id: sarah.id, grade: 80)
+world_history_sarah = CourseStudent.create!(course_id: world_history.id, student_id: sarah.id, grade: 87)
+
+math_john = CourseStudent.create!(course_id: math.id, student_id: john.id, grade: 91)
+english_john = CourseStudent.create!(course_id: english.id, student_id: john.id, grade: 80)
+world_history_john = CourseStudent.create!(course_id: world_history.id, student_id: john.id, grade: 99)

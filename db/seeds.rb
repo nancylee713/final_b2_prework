475eecb6bb6e3f42e46b61d9b56cc323bd63fe47
Nancy Lee <srlee713@gmail.com>
2019-09-13 10:45:31 -0600

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CourseStudent.destroy_all
+Course.destroy_all
+Student.destroy_all
+
+pete = Student.create!(name: "Pete")
+math = Course.create!(name: "Calculus I")
+math_pete = CourseStudent.create!(course_id: math.id, student_id: pete.id, grade: 90)
+
+english = Course.create!(name: "Compsition 101")
+english_pete = CourseStudent.create!(course_id: english.id, student_id: pete.id, grade: 88)
+
+world_history = Course.create!(name: "World History")
+world_history_pete = CourseStudent.create!(course_id: world_history.id, student_id: pete.id, grade: 93)
